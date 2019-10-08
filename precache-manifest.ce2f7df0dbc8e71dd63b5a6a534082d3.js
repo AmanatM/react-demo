4138fda890910eae570e4ec024af0d16253bc9f8
Amanat <amnaatmurzaliev@gmail.com>
2019-10-09 03:12:18 +0600

    Updates

---
+++ precache-manifest.ce2f7df0dbc8e71dd63b5a6a534082d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e709ba1ac9764b297056a986812a147",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "614613a2f69c1f572a8a",
+    "url": "/react-demo/static/js/2.90ed5f12.chunk.js"
+  },
+  {
+    "revision": "c64745df78dd7c3523e2",
+    "url": "/react-demo/static/js/main.9778ed84.chunk.js"
+  },
+  {
+    "revision": "a76f6d7941f98b96e2bc",
+    "url": "/react-demo/static/js/runtime-main.ef2669f1.js"
+  }
+]);
